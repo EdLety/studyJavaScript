981ccfc06f10485925f8d3a6a57e78bda6824a48
Nuss Eduard Andreevich <nuss.ea@arkan.ru>
2024-10-27 17:59:53 +0300

    Пройдено 2 урока из 2 раздела.

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-// alert("Hi, mate!");
+// intro
+/*alert("Hi, mate!");
 console.log("Error:404");
 confirm("Вы согласны");
 prompt("Сколько тебе лет?");
 let x = prompt("Сколько тебе лет?");
-alert(x);
+alert(x);*/
+// variables
+// 1
+/*let number = 5;
+let str = "Hello";
+console.log(str);*/
+// 2
+let num = 5;
+console.log(num);
+num = prompt("Add your num: ");
+console.log(num);
