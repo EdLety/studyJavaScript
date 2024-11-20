71c9c4aa5a0437bbe5bcc8aed03de44a75611e22
Nuss Eduard Andreevich <nuss.ea@arkan.ru>
2024-11-20 11:06:43 +0300

    Пройден этап с переменными в курсе на степике.

--- main.js
+++ main.js
@@ -11,7 +11,21 @@ alert(x);*/
 let str = "Hello";
 console.log(str);*/
 // 2
-let num = 5;
+/*let num = 5;
 console.log(num);
 num = prompt("Add your num: ");
-console.log(num);
+console.log(num);*/
+// 2.3
+/* const username = "John"; // flat case
+const userName = "John"; // camel case
+const user_name = "John"; // snake case
+const UserName = "John"; // Pascal case */
+
+// 2.4
+/* "use strict";
+
+number = 5;
+console.log(number); */
+// 2.6
+
+debugger;
